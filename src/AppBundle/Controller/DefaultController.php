755b2712a5ff4129c68139a0923de0fa12f3e77f
Ivo Lukac <ivo@netgen.hr>
2017-10-26 00:09:40 +0200

    removed var dumping

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $contentfulEntry = $this->get("app.contentful_service")->loadContentfulEntry("tt0p8zsqsjr3|A96usFSlY4G0W4kwAqswk");
-        var_dump($contentfulEntry->getName());
-
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
